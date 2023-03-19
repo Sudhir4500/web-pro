a0fe4f64ba263979d13eae1d6399292ad505b6df
sudhir sharma <sudhirsharma4500@gmail.com>
2023-03-19 20:22:38 +0545

    initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// var typed=new typed(".multiple-text",{
+//     strings:["frontend developer","game developer","backend developer","and learning...."],
+//     typeSpeed:100,
+//     backSpeed:100,
+//     backDelay:1000,
+//     loop:true
+// })
